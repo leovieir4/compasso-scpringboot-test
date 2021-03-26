e3f8ee5eebaa37caf577699a7576aafcc6f0a1f4
leoovireira <leonardo.vieira@axyma.com.br>
2021-03-26 18:52:24 -0300

    ajust swagger description

--- src/main/java/br/com/compassouol/product/ms/SwaggerConfig.java
+++ src/main/java/br/com/compassouol/product/ms/SwaggerConfig.java
@@ -24,8 +24,8 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 	}
 
 	private ApiInfo metaData() {
-		return new ApiInfoBuilder().title("Spring Boot REST API")
-				.description("\"Spring Boot REST API for greeting people\"").version("1.0.0")
+		return new ApiInfoBuilder().title("Spring Boot REST API - Compasso UOL")
+				.description("\"Spring Boot REST API for compassouol challenge\"").version("1.0.0")
 				.license("Apache License Version 2.0").licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
 				.build();
 	}
