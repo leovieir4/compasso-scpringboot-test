1ddc18d8ebddb9283c22819b50ffac7f516ab947
leoovireira <leonardo.vieira@axyma.com.br>
2021-03-28 18:08:16 -0300

    ajust price validation to not receive null value

--- src/main/java/br/com/compassouol/product/ms/model/Product.java
+++ src/main/java/br/com/compassouol/product/ms/model/Product.java
@@ -7,15 +7,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.Range;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -24,24 +23,24 @@ import lombok.Setter;
 public class Product {
 
 	@Id
-	@GeneratedValue(generator="system-uuid")
-	@GenericGenerator(name="system-uuid", strategy = "uuid")
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy = "uuid")
 	@ApiModelProperty(notes = "The product id", required = true)
 	private String id;
-	
+
 	@NotNull(message = "Name cant be null")
 	@NotEmpty(message = "Name is mandatory")
 	@ApiModelProperty(notes = "The name of the product", required = true)
 	private String name;
-	
+
 	@NotNull(message = "Description cant be null")
 	@NotEmpty(message = "Description is mandatory")
 	@ApiModelProperty(notes = "A short description to the product", required = true)
 	private String description;
-	
-    @Positive(message = "Price must be positive")
-    @ApiModelProperty(notes = "The price of the product must be positive", required = false)
-	private BigDecimal price;	
 
+	@Range(min = 0, message = "Price must be positive")
+	@ApiModelProperty(notes = "The price of the product must be positive", required = false)
+	@NotNull(message = "Price cant be null")
+	private BigDecimal price;
 
 }
